780cb0f1197f30f8e9933d1759b586c73615e526
daydream <zn.moon2016@gmail.com>
2018-02-25 21:34:36 +0800

    update code style

--- cpp/memory-control/test.cpp
+++ cpp/memory-control/test.cpp
@@ -5,75 +5,92 @@
 #include <vector>
 using namespace std;
 
-void test_malloc() {
-  auto ptr = malloc(sizeof(int) * 10);
-  free(ptr);
+void test_malloc()
+{
+    auto ptr = malloc(sizeof(int) * 10);
+    free(ptr);
 }
 
-void test_calloc() {
-  auto ptr = calloc(1, sizeof(int));
-  free(ptr);
+void test_calloc()
+{
+    auto ptr = calloc(1, sizeof(int));
+    free(ptr);
 }
 
-void test_realloc() {
-  auto ptr = malloc(sizeof(int) * 100);
-  ptr = realloc(ptr, 1000);
-  free(ptr);
+void test_realloc()
+{
+    auto ptr = malloc(sizeof(int) * 100);
+    ptr      = realloc(ptr, 1000);
+    free(ptr);
 }
 
-void test_operator_new_delete() {
-  {
-    auto ptr = operator new(10);
-    operator delete(ptr);
-  }
-  {
-    auto ptr = operator new[](10);
-    operator delete[](ptr);
-  }
+void test_operator_new_delete()
+{
+    {
+        auto ptr = operator new(10);
+                   operator delete(ptr);
+    }
+    {
+        auto ptr = operator new[](10);
+                   operator delete[](ptr);
+    }
 }
 
-void *operator new(size_t size) {
-  std::cout << "operator new called." << size << std::endl;
-  return malloc(size);
+void *operator new(size_t size)
+{
+    std::cout << "operator new called." << size << std::endl;
+    return malloc(size);
 }
-void operator delete(void *ptr) {
-  std::cout << "operator delete called." << std::endl;
-  return free(ptr);
+void operator delete(void *ptr)
+{
+    std::cout << "operator delete called." << std::endl;
+    return free(ptr);
 }
-void *operator new[](size_t size) {
-  std::cout << "operator new[] called." << size << std::endl;
-  return malloc(size);
+void *operator new[](size_t size)
+{
+    std::cout << "operator new[] called." << size << std::endl;
+    return malloc(size);
 }
-void operator delete[](void *ptr) {
-  std::cout << "operator delete[] called." << std::endl;
-  return free(ptr);
+void operator delete[](void *ptr)
+{
+    std::cout << "operator delete[] called." << std::endl;
+    return free(ptr);
 }
 
-class Object {
-public:
-  Object() { cout << "constructed" << endl; }
-  ~Object() { std::cout << "destructed" << std::endl; }
+class Object
+{
+  public:
+    Object()
+    {
+        cout << "constructed" << endl;
+    }
+    ~Object()
+    {
+        std::cout << "destructed" << std::endl;
+    }
 
-private:
-  int x;
+  private:
+    int x;
 };
 
-void test_expression_new_delete() {
-  {
-    auto x = new Object;
-    delete x;
-  }
-  {
-    auto x = new Object[10];
-    delete[] x;
-  }
+void test_expression_new_delete()
+{
+    {
+        auto x = new Object;
+        delete x;
+    }
+    {
+        auto x = new Object[10];
+        delete[] x;
+    }
 }
 
-int main(int argc, char **argv) {
-  //   test_malloc();
-  //   test_calloc();
-  //   test_realloc();
-  //   test_operator_new_delete();
-  test_expression_new_delete();
-  return 0;
+int main(int argc, char **argv)
+{
+    //   test_malloc();
+    //   test_calloc();
+    //   test_realloc();
+    //   test_operator_new_delete();
+    test_expression_new_delete();
+    return 0;
 }
